6a3073bb72f75dc6db03c1098a40634abee4a7cf
Bey <Josh@picksports.app>
2024-09-17 18:27:20 -0500

    scheduling jobs

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
+// Fetch NCAA FB data every Monday at 7:00 PM
 Schedule::command('fetch:college-football-elo')->mondays()->at('19:00');
 Schedule::command('fetch:college-football-fpi')->mondays()->at('19:05');
 Schedule::command('fetch:sp-ratings')->mondays()->at('19:10');
