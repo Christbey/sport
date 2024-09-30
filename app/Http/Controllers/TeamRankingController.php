2acf2c998ef0589d3a3fb9d333b2c2203cc3ad83
Bey <Josh@picksports.app>
2024-09-30 18:26:41 -0500

    tr

--- app/Http/Controllers/TeamRankingController.php
+++ app/Http/Controllers/TeamRankingController.php
@@ -22,13 +22,7 @@ class TeamRankingController extends Controller
     {
         return new Client([
             'base_uri' => 'https://www.teamrankings.com',
-            'timeout' => 30.0,
-            'headers' => [
-                'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
-                'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-                'Accept-Language' => 'en-US,en;q=0.9',
-                'Connection' => 'keep-alive',
-            ],
+            
         ]);
     }
 
