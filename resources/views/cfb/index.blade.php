1d73f0c69d7845749e2324a7c5740eb9a0948476
Bey <Josh@picksports.app>
2024-10-18 19:10:20 -0500

    route to cfb show added

--- resources/views/cfb/index.blade.php
+++ resources/views/cfb/index.blade.php
@@ -74,15 +74,12 @@
                                 </div>
                             @endif
                             <!-- Detail Button -->
-                            <!-- Detail Button -->
                             <div class="mt-4">
                                 <a href="{{ route('cfb.hypothetical.show', ['game_id' => $game->game_id]) }}"
                                    class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
                                     View Details
                                 </a>
                             </div>
-
-
                         </div>
                     </div>
                 @endforeach
